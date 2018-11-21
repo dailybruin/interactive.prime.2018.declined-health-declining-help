acf67f9f7d51c013e4462d1baac5eff2f577963d
legitmaxwu <chengmaxwu@gmail.com>
2018-11-20 18:54:15 -0800

    changed component name to VideoOnClick

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { Article, CoverPhoto, Footer, Head } from '@dailybruin/lux'
-import HoverVideo from '../components/HoverVideo/HoverVideo'
+import HoverVideo from '../components/VideoOnClick/VideoOnClick'
 
 export const query = graphql`
   query {
